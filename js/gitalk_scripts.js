e07aff14fb4effd79def85d35c0fca78fde48af6
蔡孟辰 <b06502020@ntu.edu.tw>
2021-07-29 01:19:24 +0800

    second section test

--- js/gitalk_scripts.js
+++ js/gitalk_scripts.js
@@ -10,7 +10,18 @@ const gt_1_1 = new Gitalk({
   distractionFreeMode: false  // Facebook-like distraction free mode
 });
 
+const gt_1_2 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 2,			  // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
 
 
 // Rendering
 gt_1_1.render('gt-container-1-1');
+gt_1_2.render('gt-container-1-2');
