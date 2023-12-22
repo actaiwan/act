19858c3199adf657399b12cc59bd0d344b357117
act-taiwan <accessiblitycodestaiwan@gmail.com>
2023-12-22 16:38:13 +0800

    update 4.1 sidebar, add issues for comment till 2.2

--- js/gitalk_scripts.js
+++ js/gitalk_scripts.js
@@ -1,5 +1,5 @@
 // Initialization
-const gt_1_1 = new Gitalk({
+const gt_111 = new Gitalk({
   clientID: '7f70459a0d8e387c0d4b',
   clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
   repo: 'repo_for_comments',      // The repository of store comments,
@@ -10,7 +10,7 @@ const gt_1_1 = new Gitalk({
   distractionFreeMode: false  // Facebook-like distraction free mode
 });
 
-const gt_1_2 = new Gitalk({
+const gt_121 = new Gitalk({
   clientID: '7f70459a0d8e387c0d4b',
   clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
   repo: 'repo_for_comments',      // The repository of store comments,
@@ -21,7 +21,416 @@ const gt_1_2 = new Gitalk({
   distractionFreeMode: false  // Facebook-like distraction free mode
 });
 
+const gt_122 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 3,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
 
+const gt_123 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 4,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_124 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 5,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_125 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 7,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_126 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 8,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_127 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 9,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_128 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 10,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_129 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 11,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+
+const gt_131 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 12,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_132 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 13,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_133 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 14,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_134 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 15,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_135 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 16,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_136 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 17,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+
+const gt_141 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 18,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_142 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 19,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_143 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 20,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_144 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 21,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_145 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 22,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_146 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 23,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_147 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 24,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_148 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 25,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_149 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 26,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_1410 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 27,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_1411 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 28,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_1412 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 29,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_1413 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 30,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_211 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 31,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_212 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 32,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_213 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 33,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_214 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 34,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_221 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 35,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_222 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 36,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_223 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 37,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_224 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 38,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_225 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 39,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
+const gt_226 = new Gitalk({
+  clientID: '7f70459a0d8e387c0d4b',
+  clientSecret: '7909f208f02050a7aa947360096d7e31563b0b4f',
+  repo: 'repo_for_comments',      // The repository of store comments,
+  owner: 'actaiwan',
+  admin: ['actaiwan'],
+  number: 40,        // Issue number
+  id: location.href,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+});
 // Rendering
-gt_1_1.render('comment-sc111');
-gt_1_2.render('comment-sc121');
+gt_111.render('comment-sc111');
+gt_121.render('comment-sc121');
+gt_122.render('comment-sc122');
+gt_123.render('comment-sc123');
+gt_124.render('comment-sc124');
+gt_125.render('comment-sc125');
+gt_126.render('comment-sc126');
+gt_127.render('comment-sc127');
+gt_128.render('comment-sc128');
+gt_129.render('comment-sc129');
+gt_131.render('comment-sc131');
+gt_132.render('comment-sc132');
+gt_133.render('comment-sc133');
+gt_134.render('comment-sc134');
+gt_135.render('comment-sc135');
+gt_136.render('comment-sc136');
+gt_141.render('comment-sc141');
+gt_142.render('comment-sc142');
+gt_143.render('comment-sc143');
+gt_144.render('comment-sc144');
+gt_145.render('comment-sc145');
+gt_146.render('comment-sc146');
+gt_147.render('comment-sc147');
+gt_148.render('comment-sc148');
+gt_149.render('comment-sc149');
+gt_1410.render('comment-sc1410');
+gt_1411.render('comment-sc1411');
+gt_1412.render('comment-sc1412');
+gt_1413.render('comment-sc1413');
+gt_211.render('comment-sc211');
+gt_212.render('comment-sc212');
+gt_213.render('comment-sc213');
+gt_214.render('comment-sc214');
+gt_221.render('comment-sc221');
+gt_222.render('comment-sc222');
+gt_223.render('comment-sc223');
+gt_224.render('comment-sc224');
+gt_225.render('comment-sc225');
+gt_226.render('comment-sc226');
